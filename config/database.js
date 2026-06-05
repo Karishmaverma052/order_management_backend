0c441b2f500c503a1d42f0796041dbadfb8f7ed1
Karishma <karishmaverma0532@gmail.com>
2026-06-05 12:51:22 +0530

    Db Updated

--- config/database.js
+++ config/database.js
@@ -5,11 +5,11 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const pool = new Pool({
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  database: process.env.DB_NAME,
+  user: 'cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc',
+  password: 'gjeJDccKX05Yysvn1VEShEWbYELrMb1C',
+  host: 'dpg-d8h7fuj7uimc73clpu4g-a',
+  port: '5432',
+  database: 'order_management_db_exeq',
 });
 
 pool.on('error', (err) => {
